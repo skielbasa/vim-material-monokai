skielbasa/vim-material-monokai

commit 66e628820acb0f30c111fe672ce00163ac75aa9c

Add option for a subtle mode indicator :sparkles:
Add option ‘materialmonokai_subtle_airline’. When enabled, only Section A (the mode indicator) will change colors based on the current mode.

Off by defualt to not disrupt people already using the theme, but this the recommend way to use the theme.

autoload/airline/themes/materialmonokai.vim
@@ -4,6 +4,10 @@
 " Forked From: Mike Hartington's Oceanicnext
 " ============================================================
 
+if ! exists("g:materialmonokai_subtle_airline")
+    let g:materialmonokai_subtle_airline = 0
+endif
+
 " {{{ Colors
   let s:base00=['#1F292D', '233']
   let s:base01=['#383a3e', '236']
@@ -46,11 +50,23 @@ let s:warning   = [s:base00[0], s:base09[0], s:base00[1], s:base09[1]]
 let s:error     = [s:base00[0], s:base08[0], s:base00[1], s:base08[1]]
 
 let g:airline#themes#materialmonokai#palette = {}
-let g:airline#themes#materialmonokai#palette.normal = airline#themes#generate_color_map(s:normal1, s:normal2, s:normal3)
-let g:airline#themes#materialmonokai#palette.insert = airline#themes#generate_color_map(s:insert1, s:insert2, s:insert3)
-let g:airline#themes#materialmonokai#palette.replace = airline#themes#generate_color_map(s:replace1, s:replace2, s:replace3)
-let g:airline#themes#materialmonokai#palette.visual = airline#themes#generate_color_map(s:visual1, s:visual2, s:visual3)
-let g:airline#themes#materialmonokai#palette.inactive = airline#themes#generate_color_map(s:inactive1, s:inactive2, s:inactive3)
+
+if g:materialmonokai_subtle_airline == 1
+  let g:airline#themes#materialmonokai#palette.normal = airline#themes#generate_color_map(s:normal1, s:normal2, s:normal3, s:normal3, s:normal2, s:normal1)
+  let g:airline#themes#materialmonokai#palette.insert = airline#themes#generate_color_map(s:insert1, s:insert2, s:insert3, s:normal3, s:normal2, s:normal1)
+  let g:airline#themes#materialmonokai#palette.replace = airline#themes#generate_color_map(s:replace1, s:replace2, s:replace3, s:normal3, s:normal2, s:normal1)
+  let g:airline#themes#materialmonokai#palette.visual = airline#themes#generate_color_map(s:visual1, s:visual2, s:visual3, s:normal3, s:normal2, s:normal1)
+  let g:airline#themes#materialmonokai#palette.inactive = airline#themes#generate_color_map(s:inactive1, s:inactive2, s:inactive3, s:normal3, s:normal2, s:normal1)
+  let g:airline#themes#materialmonokai#palette.visual.airline_warning = s:warning
+  let g:airline#themes#materialmonokai#palette.visual.airline_error = s:error
+else
+  let g:airline#themes#materialmonokai#palette.normal = airline#themes#generate_color_map(s:normal1, s:normal2, s:normal3)
+  let g:airline#themes#materialmonokai#palette.insert = airline#themes#generate_color_map(s:insert1, s:insert2, s:insert3)
+  let g:airline#themes#materialmonokai#palette.replace = airline#themes#generate_color_map(s:replace1, s:replace2, s:replace3)
+  let g:airline#themes#materialmonokai#palette.visual = airline#themes#generate_color_map(s:visual1, s:visual2, s:visual3)
+  let g:airline#themes#materialmonokai#palette.inactive = airline#themes#generate_color_map(s:inactive1, s:inactive2, s:inactive3)
+endif
+
 let g:airline#themes#materialmonokai#palette.normal.airline_warning = s:warning
 let g:airline#themes#materialmonokai#palette.normal.airline_error = s:error
 let g:airline#themes#materialmonokai#palette.insert.airline_warning = s:warning

commit 989b0e2fd9858b1da758b76fd0aa1a67ce172690

Fix Airline inactive colors :bug:
Fix bug introduced with 66e6288 that prevented inactive buffer’s Airline bars from greying out.

The casue was me being a little too libral with copy and paste while adding the new colors to the righthand side of the bar. Added them where they shouldn’t have been added.

autoload/airline/themes/materialmonokai.vim
@@ -51,12 +51,13 @@ let s:error     = [s:base00[0], s:base08[0], s:base00[1], s:base08[1]]
 
 let g:airline#themes#materialmonokai#palette = {}
 
+let g:airline#themes#materialmonokai#palette.inactive = airline#themes#generate_color_map(s:inactive1, s:inactive2, s:inactive3)
+
 if g:materialmonokai_subtle_airline == 1
   let g:airline#themes#materialmonokai#palette.normal = airline#themes#generate_color_map(s:normal1, s:normal2, s:normal3, s:normal3, s:normal2, s:normal1)
   let g:airline#themes#materialmonokai#palette.insert = airline#themes#generate_color_map(s:insert1, s:insert2, s:insert3, s:normal3, s:normal2, s:normal1)
   let g:airline#themes#materialmonokai#palette.replace = airline#themes#generate_color_map(s:replace1, s:replace2, s:replace3, s:normal3, s:normal2, s:normal1)
   let g:airline#themes#materialmonokai#palette.visual = airline#themes#generate_color_map(s:visual1, s:visual2, s:visual3, s:normal3, s:normal2, s:normal1)
-  let g:airline#themes#materialmonokai#palette.inactive = airline#themes#generate_color_map(s:inactive1, s:inactive2, s:inactive3, s:normal3, s:normal2, s:normal1)
   let g:airline#themes#materialmonokai#palette.visual.airline_warning = s:warning
   let g:airline#themes#materialmonokai#palette.visual.airline_error = s:error
 else
@@ -64,7 +65,6 @@ else
   let g:airline#themes#materialmonokai#palette.insert = airline#themes#generate_color_map(s:insert1, s:insert2, s:insert3)
   let g:airline#themes#materialmonokai#palette.replace = airline#themes#generate_color_map(s:replace1, s:replace2, s:replace3)
   let g:airline#themes#materialmonokai#palette.visual = airline#themes#generate_color_map(s:visual1, s:visual2, s:visual3)
-  let g:airline#themes#materialmonokai#palette.inactive = airline#themes#generate_color_map(s:inactive1, s:inactive2, s:inactive3)
 endif
 
 let g:airline#themes#materialmonokai#palette.normal.airline_warning = s:warning